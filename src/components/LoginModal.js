f188be5cc216551e760b05b54be618c540e61818
Jeremiasz Wisniewski <jeremiasz.wisniewski@sensilabs.pl>
2021-04-16 12:21:00 +0200

    Initial commit

---
+++ src/components/LoginModal.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const LoginModal = (props) => {
+  
+
+  const [modal, setModal] = useState(false);
+
+
+  return (
+    <div className="backdrop">
+      
+    </div>
+  );
+}
+
+export default LoginModal;
